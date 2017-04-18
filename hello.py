214439043c3ceb79394b562df2ab02e55ad38935
tomas-drozd <tomas.drozd@tul.cz>
2017-04-18 11:28:18 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import sys
 import multiprocessing as mp
 print('Hello word')
-print('Verze:',sys.version)
-print('Pocet procesoru:',mp.cpu_count())
+print('Verze: %s ',str(sys.version))
+print('Pocet procesoru: %s ',str(mp.cpu_count()))
+
+if 1:
+  print('tady')
+else:
+  print('tady nikdy')
